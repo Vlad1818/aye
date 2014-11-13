3f4ccd4c4857bb3e74cc601a9dfe492b828ae65e
Vlad Shumlin <vsh1818@gmail.com>
2014-11-13 19:29:09 +0200

    main activity

--- Aye/gen/sonic/industries/aye/R.java
+++ Aye/gen/sonic/industries/aye/R.java
@@ -22,7 +22,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_name=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int ImageButton1=0x7f060001;
+        public static final int ImageButton2=0x7f060003;
+        public static final int ImageButton3=0x7f060002;
+        public static final int ImageButton4=0x7f060004;
+        public static final int ImageButton5=0x7f060006;
+        public static final int ImageButton6=0x7f060005;
+        public static final int ImageButton7=0x7f060008;
+        public static final int ImageButton8=0x7f060007;
+        public static final int buttonchange=0x7f060009;
+        public static final int lahtzanchange=0x7f060000;
+        public static final int profilechange=0x7f06000a;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
@@ -31,6 +45,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int changebuttonsstring=0x7f050004;
+        public static final int changeprofilestring=0x7f050005;
         public static final int hello_world=0x7f050001;
+        public static final int helpbuttonsdescription=0x7f050003;
     }
 }
--- Aye/src/sonic/industries/aye/MainActivity.java
+++ Aye/src/sonic/industries/aye/MainActivity.java
@@ -1,7 +1,16 @@
 package sonic.industries.aye;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import android.R.string;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.View;
 
 
 /*
@@ -10,11 +19,105 @@ import android.os.Bundle;
  * */
 
 public class MainActivity extends Activity {
+	
+	public static String appPrefs = "SolacePref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences settings = getSharedPreferences(appPrefs, 0);
+        String profile = settings.getString("BigBtn1", "Not Found");
+        if(profile=="Not Found"){
+        	Initiallize();
+        }
+    }
+    
+    public void distressButtonHandler(View v) {
+
+    	switch(v.getId())
+    	{
+	    	case R.id.ImageButton1:
+	    		sendIntentonDistress("BigBtn1");
+	    	break;
+	    	case R.id.ImageButton2:
+	    		sendIntentonDistress("BigBtn2");
+	    	break;
+	    	case R.id.ImageButton3:
+	    		sendIntentonDistress("SmallBtn1");
+	    	break;
+	    	case R.id.ImageButton4:
+	    		sendIntentonDistress("SmallBtn4");
+	    	break;
+	    	case R.id.ImageButton5:
+	    		sendIntentonDistress("SmallBtn2");
+	    	break;
+	    	case R.id.ImageButton6:
+	    		sendIntentonDistress("SmallBtn5");
+	    	break;
+	    	case R.id.ImageButton7:
+	    		sendIntentonDistress("SmallBtn3");
+	    	break;
+	    	case R.id.ImageButton8:
+	    		sendIntentonDistress("SmallBtn6");
+	    	break;
+	    	default:
+	    	throw new RuntimeException("Unknow button ID");
+    	}
+    	
+    }
+    
+    private void sendIntentonDistress(String btn){
+        SharedPreferences settings = getSharedPreferences(appPrefs, 0);
+        String profile = settings.getString(btn, "Not Found");
+        if(profile=="Not Found"){
+        	Initiallize();
+        }
+    }
+    
+
+    public void startButtonChangeActivity(View v){
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+
+        dlgAlert.setMessage("wrong password or username");
+        dlgAlert.setTitle("Error Message...");
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+
+    }
+
+    public void startProfileChangeActivity(View v){
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+
+        dlgAlert.setMessage("wrong password or username");
+        dlgAlert.setTitle("Error Message...");
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+
+    }
+    
+    
+    private void Initiallize(){
+        SharedPreferences settings = getSharedPreferences(appPrefs, 0);
+        Editor editor = settings.edit();
+        editor.putString("BigBtn1", "A");
+        editor.putString("BigBtn2", "B");
+        editor.putString("SmallBtn1", "C");
+        editor.putString("SmallBtn2", "D");
+        editor.putString("SmallBtn3", "E");
+        editor.putString("SmallBtn4", "F");
+        editor.putString("SmallBtn5", "G");
+        editor.putString("SmallBtn6", "H");
+        
+        String profiles = "A,B,C,D,E,F,G,H,U,X";
+        editor.putString("profiles", profiles);
+        
+        editor.putInt("radius", 500);
+        
+        editor.commit(); 
     }
 
 }   
